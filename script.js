c0947d980d14846a272369dd8946f957543e7cb5
lancedussault <144727107+lancedussault@users.noreply.github.com>
2024-06-24 13:38:35 -0600

    Adding calculations!

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 const display = document.getElementById("display");
+let trailingResult = 0;
+const operationOptions = ['add', 'subtract', 'multiply', 'divide'];
+let workingOperation = '';
 
-const updateDisplay = num => {
- if (display.innerText === "0") {
-  display.innerText = num;
- } else if() {
- 
+const updateDisplay = input => {
+ if (display.innerText === "0" && operationOptions.indexOf(input) === -1) {
+  display.innerText = input;
+ } else if(operationOptions.indexOf(input) >= 0) {
+  workingOperation = input;
+  trailingResult = display.innerHTML;
+  display.innerHTML = 0;
+ } else if(input === "equals") {
+  console.log(trailingResult, display.innerHTML);
+  display.innerHTML = calculate(parseFloat(trailingResult), parseFloat(display.innerHTML), workingOperation);
+  trailingResult = display.innerHTML;
  } else {
-  display.innerText += num;
+  display.innerText += input;
  }
+}
+
+const calculate = (firstNum, secondNum, operation) => {
+ let result;
+ switch (operation) {
+  case 'add':
+   result = firstNum + secondNum;
+   break;
+  case 'subtract':
+   result = firstNum - secondNum;
+   break;
+  case 'multiply':
+    result = firstNum * secondNum;
+    break;
+  case 'divide':
+    result = firstNum / secondNum;
+    break;
+ }
+ return result;
 }
 
 const clearDisplay = () => {
